c5be1d115a36ecf6bb7ddc0f6866f5c46112849a
Manuel Jaimes <4mjaimes@gmail.com>
2019-10-22 22:51:17 -0500

    Redux Gravatar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,25 @@
 const reducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case 'SET_FAVORITE':
+      const itemExist = state.myList.find(item => item.id == action.payload.id)
+      if (itemExist) return state;
+      return {
+        ...state,
+        myList: [...state.myList, action.payload],
+      };
+    case 'DELETE_FAVORITE':
+      return{
+        ...state,
+        myList: state.myList.filter(items => items.id !== action.payload),
+      };
+    case 'LOGIN_REQUEST':
+      return{
+        ...state,
+        user: action.payload,
+      }
+    default:
+      return state;
+  }
 };
 
 export default reducer;
